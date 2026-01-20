3cb6293a1c88383cc43bee6ec76661e00dd69488
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-01-19 23:59:10 -0500

    Add additional texture formats to TextureFormat enum

--- src/main/kotlin/dev/luna5ama/shadesmith/Config.kt
+++ src/main/kotlin/dev/luna5ama/shadesmith/Config.kt
@@ -4,15 +4,50 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 enum class TextureFormat {
-    RGBA32UI,
+    // Floating-point
     RGBA32F,
     RGBA16F,
+    RG32F,
+    RG16F,
+    R11F_G11F_B10F,
+    R32F,
+    R16F,
     RGBA16,
     RGB10_A2,
     RGBA8,
+    RG16,
+    RG8,
+    R16,
+    R8,
+    RGBA16_SNORM,
+    RGBA8_SNORM,
+    RG16_SNORM,
+    RG8_SNORM,
+    R16_SNORM,
+    R8_SNORM,
+
+    // Signed integer
+    RGBA32I,
+    RGBA16I,
+    RGBA8I,
+    RG32I,
+    RG16I,
+    RG8I,
+    R32I,
+    R16I,
+    R8I,
+
+    // Unsigned integer
+    RGBA32UI,
+    RGBA16UI,
+    RGB10_A2UI,
+    RGBA8UI,
     RG32UI,
+    RG16UI,
+    RG8UI,
     R32UI,
-    R32F,
+    R16UI,
+    R8UI,
 }
 
 @Serializable
